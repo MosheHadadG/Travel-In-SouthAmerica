7afc73be9ffa47dafb4c514998d78230c2c334d3
MosheHadadG <Moshehadad500@gmail.com>
2022-06-08 17:39:48 +0300

    Styling

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ function NavBar() {
     <div className="header">
     <div className="header-navbar">
       <div className="logo">
-        <p><span className="grey"><i class="fa-solid fa-plane-departure"></i> TISA</span> Travel In SouthAmerica</p>
+        <p><span className="grey"><i class="fa-solid fa-plane-departure"></i> TISA</span> <span className="logoSmall">Travel In South America</span></p>
         {/* <h1><span className="grey">T</span>ravel&nbsp; 
         <span className="grey">I</span>n&nbsp; 
         <span className="grey">S</span>outh<span className="grey">A</span>merica</h1> */}
